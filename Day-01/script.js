0a59ecaf7d2f4d8135a14711c915daa65ae8fa39
Its-utsav <utsavdhimmar987@gmail.com>
2025-05-13 17:25:56 +0530

    Day 1

---
+++ Day-01/script.js
@@ -0,0 +1,25 @@
+
+
+
+// const start2 = performance.now();
+// const el = document.getElementById("root");
+// const end2 = performance.now();
+// console.log(`Took ${parseFloat((end2 - start2) / 1000)} second to find ${el.textContent}`)
+
+// const start = performance.now();
+// let num = 0
+// for (let index = 0; index < 1000000; index++) {
+//     num += index;
+// }
+// const end = performance.now();
+// console.log(`Took ${parseFloat((end - start) / 1000).toFixed(2)} second to calculate ${num}`)
+
+
+// const btn = document.createElement("button");
+// btn.textContent = "Click Me :)";
+// btn.onclick = function () {
+//     console.log("Hello", new Date())
+// }
+// document.getElementsByTagName("body")[0].append(btn)
+
+
